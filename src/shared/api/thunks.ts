1be0badb49ef61adb26cb1dba2f28cf827433f5a
dovran <sony.energizer@gmail.com>
2025-08-20 09:12:23 +0500

    Changed origin of mock backend to bypass CORS

--- src/shared/api/thunks.ts
+++ src/shared/api/thunks.ts
@@ -1,8 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { BirthPlace, Document, Organization } from "./contracts";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
 
 export const loadBirthPlacesThunk = createAsyncThunk<BirthPlace[]>(
   "submitForm/loadBirthPlaces",
